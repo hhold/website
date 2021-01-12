fecf73d43d9939ecd954666c69cee8d47af013d8
Hitesh Nair <htshnr@gmail.com>
2021-01-12 11:47:40 +0530

    changed favicon

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+ // <Link href="/">
+    //   <a>
+    //     Go back home
+    //   </a>
+    // </Link>
+
+
+export default function FOF() {
+  return (
+    <div>
+        lol
+    </div>
+   
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,87 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
+  console.log("So long, and thanks for all the fish.")
   return (
-    <div className={styles.container}>
+    <div className="m-10 ml-5 text-base">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Hitesh Nair</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="title" content="Hitesh Nair" />
+        <meta
+          name="description"
+          content="Product, growth, design, code, music, startups, humour."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://hiteshnair.com/" />
+        <meta property="og:title" content="Hitesh Nair" />
+        <meta
+          property="og:description"
+          content="Product, growth, design, code, music, startups, humour."
+        />
+        <meta name="author" content="Hitesh Nair" />
+
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+      <div className="flex justify-between">
+        <h1 className="mb-2">Hitesh Nair</h1>
+        <a href="https://twitter.com/htshnr" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current text-white transform hover:text-blue-300 hover:scale-125"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+          </svg>
         </a>
-      </footer>
+      </div>
+      <p className="mb-8">
+        Product, growth, design, code, music, startups, humour.
+      </p>
+      <div>
+        <span className="font-semibold">2021</span>
+        <hr className="mt-4 mb-4"></hr>
+      ???
+      </div>
+      <div className="mt-8">
+        <span className="font-semibold">2019-2020</span>
+        <hr className="mt-4 mb-4"></hr>
+        <ul className="list-disc mt-3 mb-4 ml-10">
+          <li>
+            <a
+              href="https://medium.com/@hellohitesh/i-miss-the-office-so-i-made-an-ai-write-me-new-scripts-c4a14af4dd86"
+              target="_blank"
+            >
+              Fine-tuning OpenAI's GPT-2 model to write new The Office scripts
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/p/B_P7D4ulvrL/" target="_blank">
+              Using neural machine translation to convert The Office lines into
+              Shakespearean English
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.amazon.com/Hitesh-Inc-Victory-Road-Battle/dp/B07Q9JX6L9/"
+              target="_blank"
+            >
+              An Alexa skill exploring using voice as a medium in role-playing
+              games instead of text boxes
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://colab.research.google.com/drive/1ZANC5dbVUJyinG5-IcoeQOvtIgEU5Bo6"
+              target="_blank"
+            >
+              Using markov chains to generate text from your Instagram DMs
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }

8d7f15e9058e3d0777369eee823a95e785eccee6
Hitesh Nair <htshnr@gmail.com>
2021-01-12 13:33:03 +0530

    Changed titles

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,15 @@
 import Link from "next/link";
+import Head from "next/head";
+
 
 export default function FOF() {
   return (
     <div className="container p-20 pt-40">
+      <Head>
+        <title>Hitesh Nair</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
       <div className="text-center mb-10">
         <p className="text-3xl font-semibold mb-4">Ima keep it real with u chief</p>
         <p className="text-xl font-normal">This link doesn't work, it's probably a 404.</p>

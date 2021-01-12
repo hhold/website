b56d9d0d9aa3743805d1297ef747ed8ec600bc4f
Hitesh Nair <htshnr@gmail.com>
2021-01-12 13:14:31 +0530

    export

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,19 @@
-import Link from 'next/link'
-
- // <Link href="/">
-    //   <a>
-    //     Go back home
-    //   </a>
-    // </Link>
-
+import Link from "next/link";
 
 export default function FOF() {
   return (
-    <div>
-        lol
+    <div className="container p-20 pt-40">
+      <div className="text-center mb-10">
+        <p className="text-3xl font-semibold mb-4">Ima keep it real with u chief</p>
+        <p className="text-xl font-normal">This link doesn't work, it's probably a 404.</p>
+        
+      </div>
+
+      <div className="text-white text-center font-sans text-xl">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </div>
     </div>
-   
-  )
+  );
 }
